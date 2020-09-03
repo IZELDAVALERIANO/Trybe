e3b38e727d5475506b278b56c18494c3b85ec833
IzeldaValeriano <zel.valeriano@gmail.com>
2020-09-03 16:10:23 -0300

    bloco 4, exercício 3

---
+++ exercises/exercises/4_1/4.3.js
@@ -0,0 +1,12 @@
+/*Faça um programa que retorne o maior de três números. 
+Defina no começo do programa três 
+variáveis com os valores que serão comparados.*/
+var a = 2;
+var b = 3;
+var c = 5;
+
+if( a || b > c ){
+    console.log ( " o valor de c é o maior: " + c)
+} else if ( b || c > a){
+    console.lo ( " o valor de a é maior : " + a)}
+    else  console.log( " o valor de b é o maior: " + b)
