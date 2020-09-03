c3b68c9f147495aad1a156f8bb741abc72c35962
IzeldaValeriano <zel.valeriano@gmail.com>
2020-09-03 15:55:42 -0300

    bloco 4, exercicio 2

---
+++ exercises/exercises/4_1/4.1.js
@@ -0,0 +1,16 @@
+/* cinco programas, um para cada operação aritmética básica.
+ Seu programa deve ter duas variáveis, a e b, definidas no começo com 
+ os valores que serão operados. Faça programas para:
+Adição
+Subtracão
+Multiplicação
+Divisão
+Módulo*/
+var a = 1;
+var b = 2;
+
+console.log("adição " + (a + b));
+console.log("subtração " + (b - a));
+console.log("multiplicação " + ( b * a));
+console.log ("divisão " + ( b / a));
+console.log("modulo " + (a % 2 == 0));
---
+++ exercises/exercises/4_1/4.2.js
@@ -0,0 +1,9 @@
+
+/*Faça um programa que retorne o maior de dois números. Defina no começo do
+ programa duas variáveis com os valores que serão comparados.*/
+var a = 3;
+var b = 5;
+
+if ( a > b){
+    console.log("o maior numero è " + a);}
+else {console.log (" o maior numero é " + b)};
