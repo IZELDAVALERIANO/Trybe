4c2102c7c4b7bc637e33439c23034f7a4495b037
IzeldaValeriano <zel.valeriano@gmail.com>
2020-09-03 16:21:59 -0300

    bloco 4, execicio 4

---
+++ exercises/exercises/4_1/4.4.js
@@ -0,0 +1,10 @@
+/*Faça um programa que, dado um valor definido numa variável,
+ retorne "positive" se esse valor for positivo, 
+"negative" se for negativo e "zero" caso contrário.*/
+
+var a = 5;
+if ( a > 0){
+    console.log("positivo")
+} else if (a < 0) {
+    console.log( "negativo")
+} else { console.log( "zero")}
