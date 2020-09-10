8e01a2e95134bbe80baa3a6cec55129326bd7ff7
IzeldaValeriano <zel.valeriano@gmail.com>
2020-09-10 20:29:19 -0300

    bl 4, dia 2, exercicio 9

---
+++ exercises/exercises/4_2/4_2.9.js
@@ -0,0 +1,7 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+let array = [];
+
+for (let index = 1; index <= 26; index++) {
+    array.push(index / 2) 
+}
+console.log(array)
