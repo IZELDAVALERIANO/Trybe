c9564df525f8ded8d2c505b8ede4d7333b86c9af
IzeldaValeriano <zel.valeriano@gmail.com>
2020-10-27 17:05:22 -0300

    HOF filter e map

---
+++ exercises/exercises/higher-order-functions-do-javascript-es6_2/exercise7.js
@@ -0,0 +1,80 @@
+//Encontre o nome do livro escrito pela pessoa cujo nome registrado começa 
+//com três iniciais (terminam com um ponto).
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  return books.find( element => element.author.name.split(" "). // pass 1
+  filter(word => word.endsWith(".")).length === 3).name // passo 2
+}
+
+/*  passo 1: o finde retornará o primeiro elemento autor.name que após ser separado pelo método slipt
+ será filtrado pela palavra que contenha  3 pontos ao fim. o métódo endsWitch mostra as plavras terminadas de 
+ de trás para frente */
+
+assert.deepEqual(authorWith3DotsOnName(), expected_result);
